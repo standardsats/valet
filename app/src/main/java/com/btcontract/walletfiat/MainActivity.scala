6da8290717eb0d6048df9e96a6ea30ca12e0b7c2
anton <akumaigorodski@gmail.com>
2021-12-15 11:18:35 +0200

    Fix auth issues

--- app/src/main/java/com/btcontract/walletfiat/MainActivity.scala
+++ app/src/main/java/com/btcontract/walletfiat/MainActivity.scala
@@ -15,8 +15,6 @@ import immortan.LNParams
 import immortan.crypto.Tools.{none, runAnd}
 import immortan.utils.InputParser
 import org.bitcoinj.wallet._
-import org.ndeftools.Message
-import org.ndeftools.util.activity.NfcReaderActivity
 
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
@@ -36,32 +34,19 @@ object ClassNames {
   val hubActivityClass: Class[HubActivity] = classOf[HubActivity]
 }
 
-class MainActivity extends NfcReaderActivity with BaseActivity { me =>
+class MainActivity extends BaseActivity { me =>
   lazy val legacyWalletFile = new File(getFilesDir, "Bitcoin.wallet")
 
   override def START(state: Bundle): Unit = {
     setContentView(R.layout.frag_linear_layout)
     NotificationManagerCompat.from(me).cancelAll
-    initNfc(state)
   }
 
-  // NFC AND SHARE
-
-  // This method is always run when `onResume` event is fired, should be a starting point for all subsequent checks
-  def readNdefMessage(msg: Message): Unit = runInFutureProcessOnUI(InputParser recordValue ndefMessageString(msg), proceed)(proceed)
-
-  override def onNoNfcIntentFound: Unit = {
+  override def onResume: Unit = runAnd(super.onResume) {
     val processIntent = (getIntent.getFlags & Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) == 0
     val dataOpt = Seq(getIntent.getDataString, getIntent getStringExtra Intent.EXTRA_TEXT).find(data => null != data)
-    if (processIntent) runInFutureProcessOnUI(dataOpt foreach InputParser.recordValue, proceed)(proceed) else proceed(null)
+    if (processIntent) runInFutureProcessOnUI(dataOpt.foreach(InputParser.recordValue), proceed)(proceed) else proceed(null)
   }
-
-  def onNfcStateEnabled: Unit = none
-  def onNfcStateDisabled: Unit = none
-  def onNfcFeatureNotFound: Unit = none
-  def onNfcStateChange(ok: Boolean): Unit = none
-  def readEmptyNdefMessage: Unit = proceed(null)
-  def readNonNdefMessage: Unit = proceed(null)
 
   def proceed(empty: Any): Unit = WalletApp.isAlive match {
     case false => runAnd(WalletApp.makeAlive)(me proceed null)
@@ -71,9 +56,8 @@ class MainActivity extends NfcReaderActivity with BaseActivity { me =>
       else me exitTo ClassNames.hubActivityClass
 
     case true =>
-      val step2 = if (legacyWalletFile.exists) new EnsureLegacy else new EnsureSeed
-      val step1 = if (WalletApp.useAuth) new EnsureAuth(step2) else step2
-      step1.makeAttempt
+      val step2 = if (legacyWalletFile.exists) new EnsureLegacy(new ToHub) else new EnsureSeed(new ToHub)
+      if (WalletApp.useAuth) new EnsureAuth(step2).makeAttempt else step2.makeAttempt
   }
 
   // Tor and auth
@@ -106,7 +90,7 @@ class MainActivity extends NfcReaderActivity with BaseActivity { me =>
     }.checkAuth
   }
 
-  class EnsureSeed extends Step {
+  class EnsureSeed(next: Step) extends Step {
     def makeAttempt: Unit = WalletApp.extDataBag.tryGetSecret match {
       case Failure(_: android.database.CursorIndexOutOfBoundsException) =>
         // Record is not present at all, this is probaby a fresh wallet
@@ -118,20 +102,20 @@ class MainActivity extends NfcReaderActivity with BaseActivity { me =>
 
       case Success(secret) =>
         WalletApp.makeOperational(secret)
-        me exitTo ClassNames.hubActivityClass
+        next.makeAttempt
     }
   }
 
-  class EnsureLegacy extends Step {
+  class EnsureLegacy(next: Step) extends Step {
+    private def decrypt(wallet: Wallet, pass: String) = Try {
+      val scrypt: org.bitcoinj.crypto.KeyCrypter = wallet.getKeyCrypter
+      wallet.getKeyChainSeed.decrypt(scrypt, pass, scrypt deriveKey pass)
+    }
+
     private def restoreLegacyWallet = {
       val stream = new FileInputStream(legacyWalletFile)
       val proto: Protos.Wallet = try WalletProtobufSerializer.parseToProto(stream) finally stream.close
       (new WalletProtobufSerializer).readWallet(org.bitcoinj.params.MainNetParams.get, null, proto)
-    }
-
-    private def decrypt(wallet: Wallet, pass: String) = Try {
-      val scrypt: org.bitcoinj.crypto.KeyCrypter = wallet.getKeyCrypter
-      wallet.getKeyChainSeed.decrypt(scrypt, pass, scrypt deriveKey pass)
     }
 
     def makeAttempt: Unit = {
@@ -147,8 +131,7 @@ class MainActivity extends NfcReaderActivity with BaseActivity { me =>
           SetupActivity.fromMnemonics(seed.getMnemonicCode.asScala.toList, host = me)
           val wallet = LNParams.chainWallets.makeSigningWalletParts(core, Satoshi(0L), core.walletType)
           LNParams.chainWallets = LNParams.chainWallets.withFreshWallet(wallet)
-          me exitTo ClassNames.hubActivityClass
-          legacyWalletFile.delete
+          runAnd(next.makeAttempt)(legacyWalletFile.delete)
         } getOrElse makeAttempt
       }
     }
